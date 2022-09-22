beaa8832252c458ec13f1f8f02b811d1e1205597
Francesc Vila <fvilac@gmail.com>
2022-09-22 15:06:01 +0200

    Update App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          XAL OTT
+          Hello World!
         </p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
